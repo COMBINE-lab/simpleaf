9f0994fc9684a988b2fea2dfde5de8758de1e47c
Rob Patro <rob@newton>
2022-08-01 18:52:10 -0400

    skip-ci bump doc version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022-, Dongze He, Rob Patro'
 author = 'Dongze He, Rob Patro'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.2'
+release = '0.3.0'
 
 master_doc = 'index'
 
