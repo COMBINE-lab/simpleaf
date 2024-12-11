8403336c800172a0811018a9118d513df50c8945
Rob Patro <rob@newton>
2024-12-11 00:45:04 -0500

    start filling out some options

--- src/atac/commands.rs
+++ src/atac/commands.rs
@@ -7,10 +7,28 @@ pub enum AtacCommand {
     Process(ProcessOpts),
 }
 
+/// build a piscem index over the genome for
+/// scATAC-seq mapping.
 #[derive(Args, Clone, Debug)]
 #[command(arg_required_else_help = true)]
-pub struct IndexOpts {}
+pub struct IndexOpts {
+    /// number of threads to use when running
+    #[arg(short, long, default_value_t = 16, display_order = 1)]
+    pub threads: u32,
 
+    /// the value of k to be used to construct the index
+    #[arg(
+        short = 'k',
+        long = "kmer-length",
+        default_value_t = 31,
+        display_order = 2
+    )]
+    pub kmer_length: u32,
+}
+
+/// process a scATAC-seq sample by performing
+/// mapping, barcode correction, and sorted
+/// (deduplicated) BED file generation.
 #[derive(Args, Clone, Debug)]
 #[command(arg_required_else_help = true)]
 pub struct ProcessOpts {}
